698785b3d9be4c44874b9f14c326ce77e6433748
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2025-01-12 10:04:04 -0600

    Completed the project requirements. Also added to css to make it responsive to different screen sizes.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let homescore = document.querySelector(".homeside .score-digits");
-let guestscore = document.querySelector(".homeside .score-digits");
+let homeScoreDisplay = document.getElementById("home-score");
+let homeScore = Number(homeScoreDisplay.textContent);
+let guestScoreDisplay = document.getElementById("guest-score");
+let guestScore = Number(guestScoreDisplay.textContent);
 
 const addToScore = function (team, points) {
-    console.log("test flag");
+    console.log(team, points);
+    console.log(homeScore, guestScore);
+    if (team == "home") {
+        homeScore += points;
+        homeScoreDisplay.textContent = homeScore;
+        console.log(homeScore, guestScore);
+    }
+    else if (team == "guest") {
+        guestScore += points
+        guestScoreDisplay.textContent = guestScore;
+        console.log(homeScore, guestScore);
+    };
     return;
 };
