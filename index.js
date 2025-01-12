5b5075dd59153516365a434231fd08e2c16e284b
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2025-01-11 20:50:49 -0600

    Started writing JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+let homescore = document.querySelector(".homeside .score-digits");
+let guestscore = document.querySelector(".homeside .score-digits");
+
+const addToScore = function (team, points) {
+    console.log("test flag");
+    return;
+};
